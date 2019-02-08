f5db00b46c951a5ea1b3e350970597a31ab31776
Steven Poulsen <steven@mingler.com>
2019-02-09 00:06:54 +0100

    added M3E print layout

--- src/pages/print.ts
+++ src/pages/print.ts
@@ -11,6 +11,7 @@ export class Print {
   @observable()
   private edition:string;
   private images = {};
+  private whitePictureText = {};
 
   constructor(private crewBuilderService: CrewBuilderService, private dataService: DataService, private trackingService: TrackingService, private suitConverter: SuitValueConverter, private router: AppRouter) {
     this.loadCrew();
@@ -55,6 +56,10 @@ export class Print {
     localStorage.setItem("printEdition", newValue);
   }
 
+  togglePictureTextColor(model) {
+    this.whitePictureText[model.name] = !this.whitePictureText[model.name];
+  }
+
   private setEdition():void {
     const cacheString = localStorage.getItem("printEdition");
     if (cacheString) {
@@ -81,21 +86,37 @@ export class Print {
         chars.push(characteristic);
       }
     }
-    chars.push("Rare " + (model.allowance?model.allowance:1));
+    if (this.edition === "m2e") {
+      chars.push("Rare " + (model.allowance?model.allowance:1));
+    } else if (model.allowance > 1) {
+      chars[0] += " ("+model.allowance+")";
+    }
     return chars.join(", ");
+  }
+
+  healthPosition(max:number, value:number):number {
+    const mid = (1+max) / 2.0, multiplier = .1, pow = 2;
+    if (value <= mid) {
+      return Math.pow(Math.floor(mid)-value, pow) * multiplier;
+    }
+    return Math.pow(value - Math.ceil(mid), pow)* multiplier;
   }
 
   ruleName(rule) {
     return (rule.trigger ? rule.trigger + ' ' : '') +
       (rule.suit ? '(' + this.suitConverter.toView(rule.suit,'print') + ') ' : '') +
-      (rule.name) +
+      this.capitalize(rule.name) +
       (rule.value && !rule.type ? ' +' + rule.value : '') +
       (rule.type ? ' (' + this.suitConverter.toView(rule.type,'print') + (rule.value ? "+"+rule.value:"") + ')' : '') +
       ':'
   }
 
-  loadImages() {
-    const cacheString = localStorage.getItem("images");
-    
+  capitalize(string: string) {
+    const strings: string[] = string.split(" ");
+    const result = [];
+    for (const word of strings) {
+      result.push(word.charAt(0).toUpperCase()+word.slice(1));
+    }
+    return result.join(" ");
   }
 }
