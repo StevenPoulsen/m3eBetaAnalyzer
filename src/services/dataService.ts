b2a1eeef3b10ef5485820fc4bb911f1e0a8acf1a
Steven Poulsen <steven@mingler.com>
2019-02-19 16:47:43 +0100

    version 0.8

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -9,7 +9,7 @@ interface VersionDataEntry {
 }
 
 export class DataService {
-  private appVersion: string = "0.7";
+  private appVersion: string = "0.8";
   private data: VersionDataEntry;
   public factions = {
     "arcanists": {"id":1,"displayName": "Arcanists",key:"arcanists"},
